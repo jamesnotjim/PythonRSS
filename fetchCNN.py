80d6224c1d7948c5538a5f7229a7d9fcf7de9e8f
James Martin <wheat@wheatdesign.com>
2019-08-08 10:51:54 -0500

    initial commit

--- fetchCNN.py
+++ fetchCNN.py
@@ -1,1 +1,28 @@
+# Use XML DOM to parse CNN "Top Top Stories" RSS Feed
 
+import xml.dom.minidom
+import requests
+
+def main():
+    # retrieve the XML data using the requests library
+    url ="http://rss.cnn.com/rss/cnn_topstories.rss"
+    result = requests.get(url)
+    
+    # parse the returned content into a DOM structure
+    domtree = xml.dom.minidom.parseString(result.text)
+    rootnode = domtree.documentElement 
+
+    # print some feed metadata
+    print("Root element: {0}".format(rootnode.nodeName))
+    print("Version: {0}".format(rootnode.getAttribute('version')))
+
+    # pull the items
+    items = domtree.getElementsByTagName("item")
+    print("There are {0} items:".format(items.length))
+
+    # iterate over the items
+    for item in items: 
+        print("{0}".format(item.firstChild.firstChild.nodeValue))
+
+if __name__ == "__main__":
+    main()
